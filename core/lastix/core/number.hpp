e09e62036a2642066d32340ca2a155b77d3a76bd
Thomas Zuyev <daniil.zujevs@ikat.lv>
2025-09-28 21:40:51 +0300

    add floating point aliases

--- core/lastix/core/number.hpp
+++ core/lastix/core/number.hpp
@@ -14,4 +14,16 @@ namespace lx::core {
     using i64 = int64_t;
     using usize = std::size_t;
 
+#if __SIZEOF_FLOAT__ == 4
+    using f32 = float;
+#endif
+
+#if __SIZEOF_DOUBLE__ == 8
+    using f64 = double;
+#endif
+
+#if __SIZEOF_LONG_DOUBLE__ == 16
+    using f128 = long double;
+#endif
+
 }; // namespace lx::core
